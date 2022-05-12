5f86ed4ea75c247014ba3422355981ba58154b9f
NitishMatiyara <matiyaranitish@gmail.com>
2022-05-12 15:51:13 +0530

    logo size

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
     <>
     <div>
       <div className="head">
-      <img className="logo" src="images/foodie.png" alt="" width="30%" />
+      <img className="logo" src="images/foodie.png" alt="" />
       <nav className="navbar navbar-light" >
       <div className="container-fluid">
           <a className="navbar-brand">Find Recipe</a>
